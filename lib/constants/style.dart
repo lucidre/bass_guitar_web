61051c09dfcb7ec66fd0f4194e5b8ca25f739a4b
lucidre <otitemitope6@gmail.com>
2025-03-13 12:36:07 +0100

    update

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0XFF3F2D14);
 
+
+const Color primaryColor = Color(0XFF3F2D14);
 const Color darkColor = Color(0xFF1B1B1B);
 const Color lightColor = Color(0xFFFAFAFA);
